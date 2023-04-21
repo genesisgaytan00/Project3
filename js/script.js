22d0998c4896432e52282c31856479572068000d
genesisgaytan00 <122740973+genesisgaytan00@users.noreply.github.com>
2023-04-21 01:19:36 -0500

    deleting parts
    editing the key and js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,3 @@
-// Initialize and add the map
-let map;
-
-async function initMap() {
-  // The location of Uluru
-  const position = { lat: 41.8349, lng: -87.6270 };
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
-
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
-    zoom: 4,
-    center: position,
-    mapId: "DEMO_MAP_ID",
-  });
-
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerView({
-    map: map,
-    position: position,
-    title: "Uluru",
-  });
+function initMap(){
+    
 }
-
-initMap();
-
