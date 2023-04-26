c0d1168751b5fee0f5c4339f7d113a0869a1a44e
genesisgaytan00 <122740973+genesisgaytan00@users.noreply.github.com>
2023-04-26 17:22:32 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function initMap(){
     let iit={lat:41.8349, lng:-87.6270};
     let map = new google.maps.Map(
-        document.getElementById('map'),{zoom: 4, center:iit}
+        document.getElementById('map'),{zoom: 1, center:iit}
     );
     let marker = new google.maps.Marker({position: iit, map: map})
 
