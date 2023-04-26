6e80dfb278c508eaee03100649c1857794c495b4
genesisgaytan00 <122740973+genesisgaytan00@users.noreply.github.com>
2023-04-26 17:27:33 -0500

    zoom and center

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function initMap(){
     let iit={lat:41.8349, lng:-87.6270};
     let map = new google.maps.Map(
-        document.getElementById('map'),{zoom: 10, center:iit}
+        document.getElementById('map'),{center:iit,zoom: 13}
     );
     let marker = new google.maps.Marker({position: iit, map: map})
 
