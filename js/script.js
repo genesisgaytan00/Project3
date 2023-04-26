591d003c301b691dc0153c7957e751884f0e4470
genesisgaytan00 <122740973+genesisgaytan00@users.noreply.github.com>
2023-04-26 18:06:42 -0500

    css

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-// function initMap(){
-//     let iit={lat:41.8349, lng:-87.6270};
-//     let map = new google.maps.Map(
-//         document.getElementById('map'),{center:iit,zoom: 16}
-//     );
-//     let marker = new google.maps.Marker({position: iit, map: map});
+function initMap(){
+    let iit={lat:41.8349, lng:-87.6270};
+    let map = new google.maps.Map(
+        document.getElementById('map'),{center:iit,zoom: 16}
+    );
+    let marker = new google.maps.Marker({position: iit, map: map});
 
 
 
 
-// }
+}
