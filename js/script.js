60ce097d2efeee7d2e0c1984ffe8dce4553eb6c3
genesisgaytan00 <122740973+genesisgaytan00@users.noreply.github.com>
2023-05-03 01:28:32 -0500

    delete

--- js/script.js
+++ js/script.js
@@ -6,28 +6,28 @@ function initMap(){
     let marker = new google.maps.Marker({position: iit, map: map});
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].classList.remove("active");
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].classList.add("active");
-  dots[slideIndex-1].className += " active";
-}
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].classList.remove("active");
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].classList.add("active");
+//   dots[slideIndex-1].className += " active";
+// }
