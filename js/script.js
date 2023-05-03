6f4dd338508f10bdeb22d21d8d7d9f71c1caeace
Genesis Gaytan <122740973+genesisgaytan00@users.noreply.github.com>
2023-05-03 00:41:07 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,30 @@ function initMap(){
         document.getElementById('map'),{center:iit,zoom: 16}
     );
     let marker = new google.maps.Marker({position: iit, map: map});
+}
 
+var slideIndex = 1;
+showSlides(slideIndex);
 
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].classList.remove("active");
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].classList.add("active");
+  dots[slideIndex-1].className += " active";
 }
